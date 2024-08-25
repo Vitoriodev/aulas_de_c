7954031f7325f09cfbb7c9eaee4e2d512ff7c44a
Vitorio_Augusto <160076193+Vitoriodev@users.noreply.github.com>
2024-08-25 12:16:18 -0300

    removendo o system("pause")
    não precisa usa o system("pause")

--- aulas/main.c
+++ aulas/main.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 
 int main(){
+
     printf("ola mundo\n");
-
-    printf("numero %i \n", 10);
-
+    
     //%i ou %d de inteiro
     //%f para numero com ponto flutuante 'float'
     //%c para 'um' caractere
     //%s para uma cadeia de caractere 'textos'
     //%e numero ciêntifico
     //%.<num>f para colocar uma quantidade de pontos flutuante ex: %.2f
-
-    system("pause");
+    
+    printf("\nPressione qualque tecla para finalizar");
+    getchar();
 
     return 0;
 }
