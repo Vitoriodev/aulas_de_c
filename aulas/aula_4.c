e7e9884a8ac028069c83f8496c287e0af5f9f005
Vitorio_Augusto <160076193+Vitoriodev@users.noreply.github.com>
2024-08-25 23:41:42 -0300

    tipo int
    criar um valor em uma variavel do tipo int

---
+++ aulas/aula_4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int valor;
+    int numero;
+
+    valor = 50;
+
+    printf("coloque um numero");
+    scanf("%d", &numero);
+
+    printf("\nvoce esta me devendo $%d", valor);
+
+    printf("\nnumero %d", numero);
+
+    return 0;
+}
