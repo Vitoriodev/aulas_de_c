163008985f1f56d9dabf95b75f42ca81d098780a
Vitorio_Augusto <160076193+Vitoriodev@users.noreply.github.com>
2024-09-30 21:42:43 -0300

    2.0

--- exercicio/atividade_de_aula_2.c
+++ exercicio/atividade_de_aula_2.c
@@ -1,28 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void tabela(){
+
+    printf("sejam bem vindo, selecione o numero para qual tipo de tabuada voc quer\n ");
+    printf("* = 1, + = 2, - = 3, / = 4\n");
+    
+
+}
+
 int main(){
 
     int i;
     int num, num2 = 1 ;
     int resul = 0;
     int conta = 0;
+    int caract;
 
-    printf("qual numero voce quer multiplica por dois? \n");
-    scanf("%d", &num);
+    tabela();
+    scanf("%d", &caract);
 
-    resul = num * num2++; //primeira multiplicação.
+    if(caract == 1){
+        printf("qual numero que voce quer na tabuada \n");
+        scanf("%d", &num);
+
+        resul = num * num2++; //primeira multiplicação.
 
 
 
-    for(i = 0; i < 10; i++){
+        for(i = 0; i < 10; i++){
 
-        conta++;
+            conta++;
         
-        printf("%d * %d = %d\n",num, conta, resul);
+            printf("%d * %d = %d\n",num, conta, resul);
 
-        resul = num * num2++;
+            resul = num * num2++;
+        }
+
     }
+
+    if(caract == 2){
+        printf("qual o numero que voce quer coloca na tabuada");
+        scanf("%d", &num);
+
+        resul = num + num2++; //primeira multiplicação.
+
+        for(i = 0; i < 10; i++){
+            conta++;
+
+            printf("%d + %d = %d\n",num, conta, resul);
+
+            resul = num + num2++;
+
+        }
+    }
+
+    if(caract == 3){
+        printf("qual o numero que voce quer coloca na tabuada");
+        scanf("%d", &num);
+
+        num2 = 10;
+
+        conta = 10;
+
+        resul = num - num2--; //primeira multiplicação.
+
+        for(i = 0; i < 10; i++){
+            
+            conta--;
+
+            printf("%d - %d = %d\n",num, conta, resul);
+
+            resul = num - num2--;
+
+        }
+    }
+
+    if(caract == 4){
+        printf("qual o numero que voce quer coloca na tabuada");
+        scanf("%d", &num);
+
+        resul = num / num2++; //primeira multiplicação.
+
+        for(i = 0; i < 10; i++){
+            conta++;
+
+            printf("%d / %d = %d\n",num, conta, resul);
+
+            resul = num / num2++;
+
+        }
+    }
+
+    
 
 
 
